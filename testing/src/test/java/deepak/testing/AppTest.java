7cd3d28ef44d93d1b2775db623639a75135b2287
deepakpadliya <deepakpadliya31@gmail.com>
2019-02-19 06:15:34 +0530

    New project created.

---
+++ testing/src/test/java/deepak/testing/AppTest.java
@@ -0,0 +1,14 @@
+package deepak.testing;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	@Test
+	public void AppTest() {
+		System.out.println("This is test");
+	}
+}
